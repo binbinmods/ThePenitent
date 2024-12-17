e7621568691736f941ff75f58d1658eb4291e1a1
binbinmods <binbinmods@gmail.com>
2024-12-16 22:47:58 -0500

    Removal of pid

--- ThePenitentTraits/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ThePenitent
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "com.binbin.ThePenitent";
-        public const string PLUGIN_NAME = "ThePenitent";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
